a4b55f92c44ffea6e38fd9d0a5393f74cb56de25
Bhavesh Asanabada <bhavesh.asana@outlook.com>
2025-03-19 22:28:49 -0500

    initialize spring app

---
+++ src/main/java/com/cipher_gate/CipherGateApplication.java
@@ -0,0 +1,13 @@
+package com.cipher_gate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CipherGateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CipherGateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cipher_gate/CipherGateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cipher_gate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CipherGateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
